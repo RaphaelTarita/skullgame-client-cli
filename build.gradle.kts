f56e15be1cb77cff61923719527ab3efbc8b4ae8
Raphael Tarita <raphael.tarita@gmail.com>
2024-05-27 13:27:06 +0200

    version upgrades: kotlin: 1.9.23 -> 1.9.24 kotlinx-serialization plugin: 1.9.23 -> 1.9.24 ktor: 2.3.10 -> 2.3.11 mordant: 2.5.0 -> 2.6.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,16 @@
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
-val kotlinVersion = "1.9.23"
-val ktorVersion = "2.3.10"
-val mordantVersion = "2.5.0"
+val kotlinVersion = "1.9.24"
+val ktorVersion = "2.3.11"
+val mordantVersion = "2.6.0"
 val jnativehookVersion = "2.2.2"
 val logbackVersion = "1.5.6"
 val skullgameCommonVersion = "1.0.3-SNAPSHOT"
 
 plugins {
-    kotlin("jvm") version "1.9.23"
-    kotlin("plugin.serialization") version "1.9.23"
+    kotlin("jvm") version "1.9.24"
+    kotlin("plugin.serialization") version "1.9.24"
     id("io.gitlab.arturbosch.detekt") version "1.23.6"
     application
 }
